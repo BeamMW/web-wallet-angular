539a4a1a2a3f606645974410028f8f57a1f1eb12
Alexander Braginsky <bro@beam-mw.com>
2021-06-10 09:33:32 +0300

    asset details added

--- src/app/modules/wallet/components/card/card.component.ts
+++ src/app/modules/wallet/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
 import { Asset } from '@app/models';
 import { environment } from '@environment';
 import {
@@ -6,7 +6,28 @@ import {
 } from '@app/store/selectors/wallet-state.selectors';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { AssetMetadata } from '@app/models';
+import { globalConsts } from '@app/consts';
+import { ObservableInput } from "ngx-observable-input";
+
+export interface Contact {
+  assetData: {}
+  asset_id: number
+  available: number
+  available_str: string
+  iconUrl: string
+  maturing: number
+  maturing_str: string
+  receiving: number
+  receiving_str: string
+  sending: number
+  sending_str: string,
+  isDetailsVisible: boolean
+}
+
+export enum selectorTitles {
+  ALL = 'BALANCE',
+  IN_PROGRESS = 'ASSET INFO'
+}
 
 @Component({
   selector: 'app-card',
@@ -14,21 +35,25 @@ import { AssetMetadata } from '@app/models';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  @Input() cardData: Asset;
+  @ObservableInput() @Input("status") public status$: Observable<any>;
+  // @Input() cardData: Asset;
   @ViewChild('card', {static: true}) card: ElementRef;
-
   assets$: Observable<any>;
   iconUrl: string;
-  assetData: AssetMetadata;
+  grothsValue: number;
+  values: Contact[];
+
+
+
+  isDetailsVisible: boolean;
+  // public compomentParams = {
+  //   isDetailsVisible:,
+  // }
 
   constructor(private store: Store<any>) {
     this.assets$ = this.store.pipe(select(selectAssetsInfo));
-    this.assetData = {
-      asset_name: '',
-      unit_name: '',
-      short_name: '',
-      smallest_unit_name: ''
-    }
+    this.grothsValue = globalConsts.GROTHS_IN_BEAM;
+    this.isDetailsVisible = false;
   }
 
   private hexToRgb = hex =>
@@ -37,20 +62,69 @@ export class CardComponent implements OnInit {
       .substring(1).match(/.{2}/g).map(x => parseInt(x, 16))
 
   ngOnInit(): void {
-    this.iconUrl = 
-      `${environment.assetsPath}/images/modules/wallet/components/card/${
-        this.cardData.asset_id === 0 ? 'icon-beam' : ('asset-' + this.cardData.asset_id)
-      }.svg`;
-    this.assets$.subscribe(assets => {
-      const asset = assets.find(elem => elem.asset_id === this.cardData.asset_id);
-      if (asset) {
-        this.assetData = asset.metadata;
-        if (asset.metadata.color) {
-          const rgbaValues = this.hexToRgb(asset.metadata.color);
-          this.card.nativeElement.setAttribute('style', 'background-image: linear-gradient(107deg, rgba(' + 
-            rgbaValues.join(',') + ', .3) 2%, rgba(0, 69, 143, .3) 98%)');
-        }
+    this.status$.subscribe(status => {
+      if (status.totals.length > 0) {
+        let data = status.totals.map(value => {
+          value['iconUrl'] = `${environment.assetsPath}/images/modules/wallet/components/card/${
+            value.asset_id === 0 ? 'icon-beam' : ('asset-' + value.asset_id)}.svg`;
+          this.assets$.subscribe(assets => {
+            const asset = assets.find(elem => elem.asset_id === value.asset_id);
+            if (asset) {
+              value['assetData'] = asset.metadata;
+              if (asset.metadata.color) {
+                const rgbaValues = this.hexToRgb(asset.metadata.color);
+                value['bgStyle'] = 'linear-gradient(107deg, rgba(' + 
+                  rgbaValues.join(',') + ', .3) 2%, rgba(0, 69, 143, .3) 98%)'  ;
+              }
+            } else {
+              value['assetData'] = {'unit_name': 'BEAM'}
+            }
+          }).unsubscribe();
+
+          if (this.values !== undefined) {
+            let prev = this.values.find(val => { return val.asset_id === value.asset_id });
+            value['isDetailsVisible'] = prev.isDetailsVisible ;
+          }
+
+          return value;
+        });
+
+        this.values = data;
+        
       }
     });
+    // this.iconUrl = 
+    //   `${environment.assetsPath}/images/modules/wallet/components/card/${
+    //     this.cardData.asset_id === 0 ? 'icon-beam' : ('asset-' + this.cardData.asset_id)
+    //   }.svg`;
+    // this.assets$.subscribe(assets => {
+    //   const asset = assets.find(elem => elem.asset_id === this.cardData.asset_id);
+    //   if (asset) {
+    //     this.assetData = asset.metadata;
+    //     if (asset.metadata.color) {
+    //       const rgbaValues = this.hexToRgb(asset.metadata.color);
+    //       this.card.nativeElement.setAttribute('style', 'background-image: linear-gradient(107deg, rgba(' + 
+    //         rgbaValues.join(',') + ', .3) 2%, rgba(0, 69, 143, .3) 98%)');
+    //     }
+    //   }
+    // });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // changes.prop contains the old and the new value...
+  }
+
+  public showDetails(asset) {
+    for(let item of this.values) {
+      if (asset.asset_id === item.asset_id) {
+        item.isDetailsVisible = !item.isDetailsVisible;
+      } else {
+        item.isDetailsVisible = false;
+      }
+    }
+  }
+
+  public hideDetails(asset) {
+    asset.isDetailsVisible = false;
   }
 }
